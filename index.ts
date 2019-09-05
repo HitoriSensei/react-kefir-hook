afb8c52f6b069bbca69f8e16ab039537985eab28
Piotr Hitori Bosak <piotr.hitori.bosak@gmail.com>
2019-09-05 21:20:44 +0200

    Fixes a bug where value does not get updated when deps have changed and new stream is a property

--- index.ts
+++ index.ts
@@ -1,33 +1,29 @@
-import { Event, Observable } from 'kefir'
+import { Event, Observable, Property } from 'kefir'
 import { useEffect, useMemo, useState } from 'react'
 
+function setError(e: any) {
+    console.error('useStream got error:', e)
+}
+
 export function useStream<T>(streamGen: () => Observable<T, unknown>, initialValue: T, deps?: any[]): T
+export function useStream<T>(streamGen: () => Property<T, unknown>, initialValue?: undefined, deps?: any[]): T
 export function useStream<T>(streamGen: () => Observable<T, unknown>, initialValue?: undefined, deps?: any[]): T | undefined
 export function useStream<T>(streamGen: () => Observable<T, unknown>, initialValue?: T, deps: any[] = []): T | undefined {
     const stream = useMemo(streamGen, deps)
 
-    let hasInitial = false
-    if ('_setActive' in stream) {
-        const streamPorcelain = (<any>stream)
-        streamPorcelain._setActive(true)
-        const currentEvent = (<Event<any, any>>(streamPorcelain._currentEvent))
-        if (currentEvent && currentEvent.type === 'value') {
-            hasInitial = true
-            initialValue = currentEvent.value
+    let [value, setValue] = useState<T | undefined>(() => {
+        if ('_setActive' in stream) {
+            const streamPorcelain = (<any>stream)
+            streamPorcelain._setActive(true)
+            const currentEvent = (<Event<any, any>>(streamPorcelain._currentEvent))
+            if (currentEvent && currentEvent.type === 'value') {
+                return currentEvent.value
+            }
         }
-    }
-
-    let [value, setValue] = useState<T | undefined>(initialValue)
-
-    const setError = (e: any) => {
-        console.error('useStream got error:', e)
-    }
+    })
 
     useEffect(() => {
-        return (hasInitial
-                ? stream.skip(1)
-                : stream
-        ).observe(setValue, setError).unsubscribe
+        return stream.observe(setValue, setError).unsubscribe
     }, deps)
 
     return value
